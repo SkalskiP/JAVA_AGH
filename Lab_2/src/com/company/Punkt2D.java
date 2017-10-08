0a5ef0a836126535c1a5e375ff22806693cd8b03
SkalskiP <pskalski@student.agh.edu.pl>
2017-10-09 00:25:38 +0200

    lab_2 exercise_3 class Punkt2D

---
+++ Lab_2/src/com/company/Punkt2D.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Punkt2D {
+
+    // Class fields
+    private double x;
+    private double y;
+
+    // Constructor
+    public Punkt2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    // Setters
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    // Method that returns distance between current point and point given as argument
+    public double distance(Punkt2D other) {
+        return Math.sqrt(Math.pow(this.getX() - other.getX(), 2) + Math.pow(this.getY() - other.getY(), 2));
+    }
+}
