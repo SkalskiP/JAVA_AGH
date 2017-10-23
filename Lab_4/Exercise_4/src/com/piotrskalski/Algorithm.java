163d3f1b9d62acab96d240ff196a37a9220bc021
SkalskiP <pskalski@student.agh.edu.pl>
2017-10-23 22:02:10 +0200

    Lab 4 Exercise 4 started

---
+++ Lab_4/Exercise_4/src/com/piotrskalski/Algorithm.java
@@ -0,0 +1,11 @@
+package com.piotrskalski;
+
+// interface for encryption classes
+public interface Algorithm {
+
+    // single word encoding method
+    String crypt(String word);
+
+    // single word decrypting method
+    String decrypt(String cryptedWord);
+}
---
+++ Lab_4/Exercise_4/src/com/piotrskalski/Rot11.java
@@ -0,0 +1,5 @@
+package com.piotrskalski;
+
+public class Rot11 {
+
+}
