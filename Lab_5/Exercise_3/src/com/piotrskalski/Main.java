aec3e11aba19ef501725fad1d6bf123620c7259f
SkalskiP <pskalski@student.agh.edu.pl>
2017-10-29 12:04:30 +0100

    test file added, exceptions types added, format validation added

--- Lab_5/Exercise_3/src/com/piotrskalski/Main.java
+++ Lab_5/Exercise_3/src/com/piotrskalski/Main.java
@@ -1,8 +1,20 @@
 package com.piotrskalski;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    MicroDvd subtitles_operator = new MicroDvd();
+
+        try {
+            subtitles_operator.delay(args[0], args[1], 5000, 25);
+        } catch (InvalidMethodArgumentsException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvalidSubtitlesFormatException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Lab_5/Exercise_3/src/com/piotrskalski/MicroDvd.java
+++ Lab_5/Exercise_3/src/com/piotrskalski/MicroDvd.java
@@ -1,15 +1,19 @@
 package com.piotrskalski;
 
-// allows you to open files
 import java.io.File;
-// generic exception indicating missing file
 import java.io.FileNotFoundException;
-
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MicroDvd {
+
+    // constant pattern for matching
+    private static final Pattern PATTERN = Pattern.compile("\\{([0-9]*)}\\{([0-9]*)}([^\\r\\n]*)");
+
+    // =================================================================================================================
 
     // method that allows delay (or acceleration) of the subtitle by the specified number of milliseconds
     public void delay(String inputPath, String outputPath, int delay, int fps)  throws InvalidMethodArgumentsException,
@@ -21,10 +25,35 @@ public class MicroDvd {
 
         // object that handles reading from a file
         Scanner fileReader = new Scanner(new FileReader(inputFile));
+
         // an object that handles writing to a file
         PrintWriter fileWriter = new PrintWriter(outputFile);
 
-        
+        // variable declaration that will hold the current line
+        String currentLine;
+
+        // looping over input file
+        while(fileReader.hasNextLine()){
+
+            // extracting a new line from the text
+            currentLine = fileReader.nextLine();
+            // building an matcher object that will help us perform the operations on the pattern
+            Matcher matcher = PATTERN.matcher(currentLine);
+
+            // validation whether current line fits to pattern
+            if(!matcher.matches()) {
+                throw new InvalidSubtitlesFormatException();
+            }
+
+            // extracting start and end frame numbers
+            int startFrame = Integer.parseInt(matcher.group(1));
+            int endFrame = Integer.parseInt(matcher.group(2));
+
+            // validation whether order of frames is correct
+            if(startFrame > endFrame) {
+
+            }
+        }
 
         // closing files streams
         fileReader.close();
