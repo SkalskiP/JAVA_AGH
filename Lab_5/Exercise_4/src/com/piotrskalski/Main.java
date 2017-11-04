59e3f982d565893b9557d5e2cd2050fcab96766e
SkalskiP <pskalski@student.agh.edu.pl>
2017-11-04 12:32:26 +0100

    Project update classyfy method rebuild, part of code that handles finpyt folder, and photo validation put into separate method

--- Lab_5/Exercise_4/src/com/piotrskalski/Main.java
+++ Lab_5/Exercise_4/src/com/piotrskalski/Main.java
@@ -3,21 +3,27 @@ package com.piotrskalski;
 import io.indico. *;
 import io.indico.api.utils.IndicoException;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        PicturesClassificator classificator = new PicturesClassificator();
-
         try {
-            classificator.classify(args[0], args[1]);
-        } catch (FileIsNotPictureException e ) {
-            e.printStackTrace();
-        } catch (GivenFolderDoesNotExistException e) {
-            e.printStackTrace();
+            PicturesClassificator classificator = new PicturesClassificator("162e5af316869ead56c13fafb5462658");
+
+            try {
+                classificator.classify(args[0], args[1]);
+            } catch (FileIsNotPictureException e ) {
+                e.printStackTrace();
+            } catch (GivenFolderDoesNotExistException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         } catch (IndicoException e) {
             e.printStackTrace();
         }
-
     }
 }
--- Lab_5/Exercise_4/src/com/piotrskalski/PicturesClassificator.java
+++ Lab_5/Exercise_4/src/com/piotrskalski/PicturesClassificator.java
@@ -1,44 +1,85 @@
 package com.piotrskalski;
 
-import io.indico. *;
-import io.indico.api.results.IndicoResult;
+import io.indico.Indico;
+import io.indico.api.results.BatchIndicoResult;
 import io.indico.api.utils.IndicoException;
+
 import org.apache.commons.io.FilenameUtils;
-
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
+// The class that handles photo classification
 public class PicturesClassificator {
 
-    String[] allowedExtensions = {"jpg", "gif", "jpeg"};
+    // An array that holds admissible file extensions
+    private String[] allowedExtensions = {"jpg", "gif", "jpeg"};
+    // The api instance that will be used for classification
+    private Indico apiInstance;
 
+    // =================================================================================================================
+
+    // Constructor
+    public PicturesClassificator(String apiKey) throws IndicoException {
+        this.apiInstance =  new Indico(apiKey);
+    }
+
+    // =================================================================================================================
+
+    // The main method that handles photos sorting
     public void classify(String inputPath, String outputPath) throws FileIsNotPictureException,
                                                                 GivenFolderDoesNotExistException,
+                                                                IOException,
                                                                 IndicoException
     {
-        Indico indico = new Indico("162e5af316869ead56c13fafb5462658");
+        // Input and output handles
+        File inputFolder = new File(inputPath);
+        File outputFolder = new File(outputPath);
 
-        System.out.println(inputPath);
-        System.out.println(outputPath);
+        // Array that holds paths to all photos that we want to classify
+        String[] pathList = getPathsList(inputFolder);
 
-        File f = new File(inputPath);
-        // verify that the folder exists
-        if(!f.exists() || !f.isDirectory()) throw new GivenFolderDoesNotExistException("Given path does not lead to existing directory.");
-
-        for (final File fileEntry : f.listFiles()) {
-
-            String fileName = fileEntry.getName();
-            String ext = FilenameUtils.getExtension(fileName);
-
-            if(!is_allowed(ext)) throw new FileIsNotPictureException("Given file is not a picture");
-            String filePath = inputPath + fileName;
-
-            IndicoResult single = indico.contentFiltering.predict(filePath);
-
-        }
+        BatchIndicoResult multiple = this.apiInstance.imageRecognition.predict(pathList);
+        List<Map<String, Double>> imageRecognition = multiple.getImageRecognition();
 
     }
 
-    public boolean is_allowed(String extension) {
+    // =================================================================================================================
+
+    private String[] getPathsList(File inputFolder) throws GivenFolderDoesNotExistException,
+                                                            FileIsNotPictureException
+    {
+
+        // Verify whether input folder exists
+        if(!inputFolder.exists() || !inputFolder.isDirectory()) throw new GivenFolderDoesNotExistException("Given path does not lead to existing directory.");
+
+        // Array that holds input folder content
+        File[] inputFolderContent = inputFolder.listFiles();
+
+        // Array that will hold paths to all photos that we want to classify
+        String[] pathList = new String[inputFolderContent.length];
+
+        // Looping over input folder content
+        for (int i = 0; i < inputFolderContent.length; i++) {
+
+            // Current file name
+            String fileName = inputFolderContent[i].getName();
+            // Current file extension
+            String ext = FilenameUtils.getExtension(fileName);
+
+            // Verify if current file extension is allowed
+            if(!isAllowed(ext)) throw new FileIsNotPictureException("Given file is not a picture");
+
+            pathList[i] = inputFolderContent[i].getAbsolutePath();
+        }
+
+        return pathList;
+    }
+
+    // =================================================================================================================
+
+    private boolean isAllowed(String extension) {
         for(String str : allowedExtensions) {
             if(str.trim().contains(extension))
                 return true;
